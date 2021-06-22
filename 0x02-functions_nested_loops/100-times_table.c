cf32e8bc2edaeaaedf541b664e75ca5e7f89aef7
wealthjerry <afolabijeremayia@gmail.comgit config --global user.email afolabijeremayia@gmail.comgit config --global user.email afolabijeremayia@gmail.com>
2021-06-22 05:51:26 -0700

    loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,54 +1,47 @@
 #include "holberton.h"
 /**
- *print_times_table - n times table.
- *@n: the number defines the table.
- *
- *Return: no return value
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
  */
 void print_times_table(int n)
 {
-	int i, j, k,product;
-	j = n + 1;
-	for (i = 0; i < j; i++)
-	{
-		for (k = 0; k < j; k++)
-		{
-			if (n > 15)
-			{
-				break;
-			}
-			else
-			{
-				product = i * k;
-				if (k == 0)
-		       		{
-					_putchar('0' + product);
-				}
-				else if (product >= 10)
-				{
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' +  (product % 10));
-				}
-				else if (product >= 100)
-				{
-	       
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
-				}
-				if (k != n)
-				{
-					_putchar(',');
-				}
-			}
-		}
-		_putchar('\n');
-	}
+int row;
+int column;
+int product;
+if (n >= 0 && n <= 15)
+{
+for (row = 0; row <= n; row++)
+{
+for (column = 0; column <= n; column++)
+{
+product = (row * column);
+if (column == 0)
+_putchar('0' + product);
+else
+{
+_putchar(',');
+_putchar(' ');
+if (product <= 9)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + product);
+}
+else if (product > 9 && product < 100)
+{
+_putchar(' ');
+_putchar('0' + (product / 10));
+_putchar('0' + (product % 10));
+}
+else if (product >= 100)
+{
+_putchar('0' + (product / 100));
+_putchar('0' + ((product / 10) % 10));
+_putchar('0' + (product % 10));
+}
+}
+}
+_putchar('\n');
+}
+}
 }
