503d60d167cdf4178acdd8a5b790394ccf554324
wealthjerry <afolabijeremayia@gmail.comgit config --global user.email afolabijeremayia@gmail.comgit config --global user.email afolabijeremayia@gmail.com>
2021-06-24 02:26:34 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,88 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ *_isupper - checks for uppercase characters
+ *@c: integer to print
+ *Return: return 1 if uppercase and 0 if otherwise
+ */
+int _isupper(int c);
+
+/**
+ *_isdigit - checks for a digit
+ *@c: integer to print
+ *Return: return 1 if a digit0 if otherwise
+ */
+int _isdigit(int c);
+
+/**
+ *mul - to multiply 2 integers
+ *@a: integer to use
+ *@b: integer to use
+ *Return: product of a*b
+ */
+int mul(int a, int b);
+
+/**
+ *print_numbers - print numbers from 0-9
+ *Return: 0
+ */
+void print_numbers(void);
+
+/**
+ *print_most_numbers - print numbers 0-9 except 2 and 4
+ *Return: always 0
+ */
+void print_most_numbers(void);
+
+/**
+ *more_numbers - prints 10x
+ *Return: always 0
+ */
+void more_numbers(void);
+
+/**
+ *print_line - to print a line in the terminal
+ *@n: integer to print
+ *Return: always 0
+ */
+void print_line(int n);
+
+/**
+ *print_diagonal -  draw a diagonal line in the terminal
+ *@n: no. of tines to print
+ *Return: always 0
+ */
+void print_diagonal(int n);
+
+/**
+ *print_square - prints a square
+ *@size: size of the square
+ *Return: always 0
+ */
+void print_square(int size);
+
+/**
+ *print_triangle - prints a triangle
+ *@size: size of the triangle
+ *Return: always 0
+ */
+void print_triangle(int size);
+
+/**
+ *print_number - prints an integer
+ *@n: integer to print
+ *Return: always 0
+ */
+void print_number(int n);
+
+#endif /* _HOLBERTON_H_ */
