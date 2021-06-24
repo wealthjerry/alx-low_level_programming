cda36e90b8054de2b87c05ce5fb0fb1093caff86
wealthjerry <afolabijeremayia@gmail.comgit config --global user.email afolabijeremayia@gmail.comgit config --global user.email afolabijeremayia@gmail.com>
2021-06-24 06:23:19 -0700

    nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,37 +1,37 @@
 #include "holberton.h"
 
 /**
- *print_number - print an integer into the standard output
- *@n: - the integer to print
- *Return: 0
+ * print_number - function that prints an integer..
+ * @n: input value to check
+ * Return: nothing.
  */
 
 void print_number(int n)
 {
-unsigned int tens, digit, positive = n;
-double t_begin = 1;
-if (n == 0)
-{
-_putchar('0');
-}
-else
-{
+unsigned int num, num2;
+int i;
+int aux = 1;
+
 if (n < 0)
 {
-positive = n * -1;
+n = n * -1;
 _putchar('-');
 }
-while (t_begin <= positive)
+num = n;
+num2 = num;
+if (num > 9)
 {
-t_begin *= 10;
-}
-tens = t_begin / 10;
-while(tens >= 1)
+while (num >= 10)
 {
-digit = positive / tens;
-_putchar(digit + '0');
-positive = (positive - (tens * digit));
-tens /= 10;
+aux = aux * 10;
+num = num / 10; 
 }
+_putchar((num2 / aux) + '0');
+aux = aux / 10;
+
+for (i = aux; i >= 1; i = i / 10)
+_putchar((num2 / i) % 10 + '0');
 }
+else
+_putchar(num + '0');
 }
